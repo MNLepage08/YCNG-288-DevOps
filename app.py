f313ad2e84d19129da8550dc4436d1e1d1dba211
MNLepage08 <mnlepage08@gmail.com>
2023-06-07 00:17:24 -0400

    add code in app

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return f'Hello'
+    return f'Hello it me'
+
+
+if __name__ == '__main__':
+    # Used when running locally only. When deploying to Cloud Run,
+    # a webserver process such as Gunicorn will serve the app.
+    app.run(host='localhost', port=8080, debug=True)
 
