c50d52bdf9c9bc271cbdf6c0e99654ef3021b8c3
MNLepage08 <mnlepage08@gmail.com>
2023-07-01 18:46:12 -0400

    Add 3 components

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
+import git
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return f'Hello it me'
+    return f'Hello world'
+
+
+@app.route('/getversion/')
+def getversion():
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+
+    return f'{sha}\n'
 
 
 if __name__ == '__main__':
     # Used when running locally only. When deploying to Cloud Run,
     # a webserver process such as Gunicorn will serve the app.
     app.run(host='localhost', port=8080, debug=True)
-
